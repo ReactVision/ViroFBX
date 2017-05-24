3d67595dd00861f9d7a65b8dab6263162b1ae11d
Raj Advani <radvani@viromedia.com>
2017-05-24 11:11:06 -0700

    VRX format for Viro models

--- macos/ViroFBX/main.m
+++ macos/ViroFBX/main.m
@@ -12,26 +12,24 @@
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
       std::string heartFBX = "/Users/radvani/Source/ViroFBX/macos/ViroFBX/heart_beat_triangles.fbx";
-      std::string heartProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/heart.proto";
+      std::string heartProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/heart.vrx";
       
       std::string minionFBX = "/Users/radvani/Source/ViroFBX/macos/ViroFBX/Hyperspace_Madness_Killamari_Minion.fbx";
-      std::string minionProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/minion.proto";
+      std::string minionProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/minion.vrx";
       
       std::string svenFBX = "/Users/radvani/Source/ViroFBX/macos/ViroFBX/Hyperspace_Madness_Sven.fbx";
-      std::string svenProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/sven.proto";
+      std::string svenProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/sven.vrx";
 
       std::string wormFBX = "/Users/radvani/Source/ViroFBX/macos/ViroFBX/worm_test.fbx";
-      std::string wormProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/worm.proto";
+      std::string wormProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/worm.vrx";
       
       std::string foxFBX = "/Users/radvani/Source/ViroFBX/macos/ViroFBX/fox.fbx";
-      std::string foxProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/fox.proto";
+      std::string foxProto = "/Users/radvani/Source/ViroRenderer/ios/ViroSample/fox.vrx";
 
       VROFBXExporter *exporter = new VROFBXExporter();
-//    exporter->exportFBX(heartFBX, heartProto);
+      exporter->exportFBX(heartFBX, heartProto);
       exporter->exportFBX(minionFBX, minionProto);
-//    exporter->debugPrint(minionFBX);
-//    exporter->debugPrint(foxFBX);
-//    exporter->exportFBX(svenFBX, svenProto);
+      exporter->exportFBX(svenFBX, svenProto);
       exporter->exportFBX(wormFBX, wormProto);
       exporter->exportFBX(foxFBX, foxProto);
       
